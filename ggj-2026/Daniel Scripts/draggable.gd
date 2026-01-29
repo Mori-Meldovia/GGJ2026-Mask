9f9d73531bf01a8caff91d1f1cc52080929cf2dc
Nobodynew2 <danielarob64@gmail.com>
2026-01-29 11:26:46 -0500

    more physics change

--- ggj-2026/Daniel Scripts/draggable.gd
+++ ggj-2026/Daniel Scripts/draggable.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var held: bool = false;
 @onready var velocity: Vector2 = Vector2(0, 0)
+@onready var resistance: float = .2
 
 @export var max_velocity: float = 10;
 @export var weight: float = 10;
@@ -13,7 +14,54 @@ func _draw() -> void:
 	
 func _physics_process(_delta: float) -> void:
 	var mouse_position = get_viewport().get_mouse_position()
-	queue_redraw()
+	
+			
+	if held == true:
+		#moves the thig directly to the thing
+		#if distance between mouse and held object and mouse is
+		#greater than radius (circle_size) pull towards object depending on distance
+		
+		stop_on_screen_edge()
+		
+		shitty_physics_a(mouse_position)
+		
+		resistance_all_directions(mouse_position)
+		
+		
+		velocity.y += .1
+		
+			
+		
+	if Input.is_action_just_released("Click"):
+		held = false
+	#physics a
+	position += velocity 
+	
+func resistance_all_directions(mouse_position) -> void:
+	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size + 5):
+			if (velocity.x > 0):
+				velocity.x -= resistance
+			if (velocity.x < 0):
+				velocity.x += resistance
+			if (velocity.y > 0):
+				velocity.y -= resistance
+			if (velocity.y < 0):
+				velocity.y += resistance
+	
+func shitty_physics_b(mouse_position) -> void:
+	var pull_vector = Vector2(cos(get_angle_to(mouse_position)), sin(get_angle_to(mouse_position)))
+	
+	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size):
+		pull_vector *= position.distance_to(mouse_position) * .005
+		velocity += pull_vector
+func shitty_physics_a(mouse_position) -> void:
+	var pull_vector = Vector2(cos(get_angle_to(mouse_position)), sin(get_angle_to(mouse_position)))
+	
+	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size):
+		pull_vector *= position.distance_to(mouse_position) * .005
+		velocity += pull_vector
+	
+func stop_on_screen_edge() -> void:
 	if (position.x < 0):
 		position.x = 0
 		velocity.x = 0
@@ -26,24 +74,9 @@ func _physics_process(_delta: float) -> void:
 	if (position.y > get_viewport().get_visible_rect().size.y):
 		position.y = get_viewport().get_visible_rect().size.y - 1
 		velocity.y = 0
-			
-	if held == true:
-		#moves the thig directly to the thing
-		#if distance between mouse and held object and mouse is
-		#greater than radius (circle_size) pull towards object depending on distance
-		
-		#if (position.distance_to(mouse_position) > GlobalDaniel.circle_size):
-		var pull_vector = Vector2(cos(get_angle_to(mouse_position)), sin(get_angle_to(mouse_position)))
-		velocity += pull_vector
-		
-			
-		
-	if Input.is_action_just_released("Click"):
-		held = false
-		velocity 
-	position += velocity
-
+	
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
 		held = true;
+		
