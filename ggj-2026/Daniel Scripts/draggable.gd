a361024eb835398326d28a53150a4b4493820acc
Nobodynew2 <danielarob64@gmail.com>
2026-01-29 22:06:09 -0500

    changed the collision code to be utilized on rigid bodies
    The rigid bodies allow godot to simulate collision between other draggable objects and walls so i dont gotta do allat

--- ggj-2026/Daniel Scripts/draggable.gd
+++ ggj-2026/Daniel Scripts/draggable.gd
@@ -1,82 +1,55 @@
-extends Node2D
+extends RigidBody2D
+class_name DraggableRigidBody
+
+@export_group("Physics")
+@export var default_gravity_scale = 1.3
+@export var pull_force: float = 8
+@export var rotation_mult: float = .0005
 
 @onready var held: bool = false;
 @onready var velocity: Vector2 = Vector2(0, 0)
-@onready var resistance: float = .2
-
-@export var max_velocity: float = 10;
-@export var weight: float = 10;
 
 
-func _draw() -> void:
-	if (Input.is_action_pressed("Click")):
-		draw_circle(to_local(get_viewport().get_mouse_position()),  GlobalDaniel.circle_size, Color.RED);
+func _ready() -> void:
+	gravity_scale = default_gravity_scale
 	
 func _physics_process(_delta: float) -> void:
 	var mouse_position = get_viewport().get_mouse_position()
 	
-			
+	
 	if held == true:
 		#moves the thig directly to the thing
 		#if distance between mouse and held object and mouse is
 		#greater than radius (circle_size) pull towards object depending on distance
 		
-		stop_on_screen_edge()
-		
-		shitty_physics_a(mouse_position)
-		
-		resistance_all_directions(mouse_position)
 		
 		
-		velocity.y += .1
-		
-			
-		
+		shitty_physics_b(mouse_position)
 	if Input.is_action_just_released("Click"):
 		held = false
+		gravity_scale = default_gravity_scale
 	#physics a
 	position += velocity 
 	
-func resistance_all_directions(mouse_position) -> void:
-	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size + 5):
-			if (velocity.x > 0):
-				velocity.x -= resistance
-			if (velocity.x < 0):
-				velocity.x += resistance
-			if (velocity.y > 0):
-				velocity.y -= resistance
-			if (velocity.y < 0):
-				velocity.y += resistance
 	
 func shitty_physics_b(mouse_position) -> void:
-	var pull_vector = Vector2(cos(get_angle_to(mouse_position)), sin(get_angle_to(mouse_position)))
+	var direction = (mouse_position - global_position).normalized()
+	var distance = global_position.distance_to(mouse_position)
 	
-	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size):
-		pull_vector *= position.distance_to(mouse_position) * .005
-		velocity += pull_vector
-func shitty_physics_a(mouse_position) -> void:
-	var pull_vector = Vector2(cos(get_angle_to(mouse_position)), sin(get_angle_to(mouse_position)))
-	
-	if (position.distance_to(mouse_position) > GlobalDaniel.circle_size):
-		pull_vector *= position.distance_to(mouse_position) * .005
-		velocity += pull_vector
-	
-func stop_on_screen_edge() -> void:
-	if (position.x < 0):
-		position.x = 0
-		velocity.x = 0
-	if (position.y < 0):
-		position.y = 0
-		velocity.y = 0
-	if (position.x > get_viewport().get_visible_rect().size.x):
-		position.x = get_viewport().get_visible_rect().size.x - 1
-		velocity.x = 0
-	if (position.y > get_viewport().get_visible_rect().size.y):
-		position.y = get_viewport().get_visible_rect().size.y - 1
-		velocity.y = 0
-	
+	if (distance > 1.5 * GlobalDaniel.circle_size):
+		var pull = direction * (distance) * pull_force
+		linear_velocity = pull
+		angular_velocity += pull.cross(Vector2.UP) * rotation_mult
+	elif (distance > GlobalDaniel.circle_size && 
+	distance < 1.5 * GlobalDaniel.circle_size):
+		var pull = direction * (distance) * (pull_force / 2)
+		linear_velocity = pull
+		angular_velocity += pull.cross(Vector2.UP) * rotation_mult
+	elif (position.distance_to(mouse_position) < GlobalDaniel.circle_size):
+		linear_velocity *= .95
 
-func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+
+func _on_input_event(_viewport: Node, event: InputEvent,_shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
 		held = true;
-		
+		#gravity_scale = 0;
